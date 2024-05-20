e062cbf603cfeb59a0116a3f7a97abf35f9b817e
baptistehelen <148351322+baptistehelen@users.noreply.github.com>
2024-05-20 23:44:34 +0200

    Correction erreur key dans le slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -23,59 +23,64 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  // ** CORRECTION ORDRE DES EVENEMENTS (PLUS ANCIEN AU PLUS RECENT) ">"
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
-  );
+  const byDateDesc =
+    data && data.focus
+      ? data.focus.sort((evtA, evtB) =>
+          new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+        )
+      : [];
+
   // ** CORRECTION PAGE BLANCHE "-1"
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length- 1 ? index + 1 : 0),
       5000
     );
   };
+
   useEffect(() => {
-    nextCard();
-  });
+    if (byDateDesc.length > 0) {
+      nextCard();
+    }
+  }, [byDateDesc]);
 
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${
+            index === idx ? "display" : "hide"
+          }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {/* ** CORRECTION "checked={index === radioIdx}" (avant idx) */}
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  // ou provided a `checked` prop to a form field without an `onChange` handler.
-                  // This will render a read-only field. If the field should be mutable use `defaultChecked`.
-                  // Otherwise, set either `onChange` or `readOnly`.
-                  readOnly
-                  key={_.id}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
+        // correction fermeture méthode map
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {/* ** CORRECTION "checked={index === radioIdx}" (avant idx) + ajout byDateDesc'?' + 'event.title' */}
+          {byDateDesc?.map((event, radioIdx) => (
+            <input
+              // ou provided a `checked` prop to a form field without an `onChange` handler.
+              // This will render a read-only field. If the field should be mutable use `defaultChecked`.
+              // Otherwise, set either `onChange` or `readOnly`.
+              readOnly
+              key={event.title}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
