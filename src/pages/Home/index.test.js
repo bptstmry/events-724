69f31d9ff84a5bb0b3069c97472ce1cd77736543
baptistehelen <148351322+baptistehelen@users.noreply.github.com>
2024-05-22 10:11:04 +0200

    Correction 'typos'

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -7,7 +7,7 @@ describe("When Form is created", () => {
     await screen.findByText("Email");
     await screen.findByText("Nom");
     await screen.findByText("Prénom");
-    await screen.findByText("Personel / Entreprise");
+    await screen.findByText("Personnel / Entreprise");
   });
 
   describe("and a click is triggered on the submit button", () => {
